f3b00ae976b80ab0d7f7e588fcffb99432901811
neighborpil <neighborpil@naver.com>
2019-01-04 13:44:26 +0900

    making bikeshop

---
+++ WpfBasicControlApp1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBasicControlApp1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            init();
+            
+        }
+
+        public void init()
+        {
+            //NavigationService.Navigate(
+            //    new Uri("/Payment.xaml", UriKind.Relative)); // 안되는데 이유를 모르겠음
+        }
+    }
+}
